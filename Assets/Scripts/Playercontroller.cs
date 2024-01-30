6c716d99e061d5978e9b853b23e9538ca4dfb0bc
Leo Zöger <leo.zoger@learnet.se>
2024-01-30 10:54:30 +0100

    e

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -7,10 +7,9 @@ using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour
 {
-   
-  
+
+
     public float speed = 5;
-    private Rigidbody rb;
     //Vector3 StartScale = Vector3.one;
     private void Start()
     {
@@ -19,28 +18,13 @@ public class Playercontroller : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        rb = GetComponent<Rigidbody>();
 
         Vector2 Movement = new Vector2(horizontalInput, 0f);
 
-        if (horizontalInput < 0)
-        {
-            FlipCharacter(-1f);
-        }
-        else if (horizontalInput < 0)
-        {
-            FlipCharacter(1f);
-        }
+        transform.Translate(Movement * speed * Time.deltaTime);
     }
-       void FlipCharacter(float direction)
-       {
-            Vector3 scale = transform.localScale;
-            scale.x = direction;
-            transform.localScale = scale;
-       }
-    }
-   
-      
+}
+
 
 
 
