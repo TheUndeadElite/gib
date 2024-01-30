7b863e1255952cd37be3400a68c588a383e922b6
Abas Yosef <abas.yosef@learnet.se>
2024-01-30 09:17:06 +0100

    jcnkd

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour
 {
    
-    lol
+  
 }
