2f09a3c3cc838a627267b6b706b44436bbc61300
Leo Zöger <leo.zoger@learnet.se>
2024-02-02 10:44:17 +0100

    flip och movement funkar

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -3,25 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour
 {
+    Rigidbody2D rb;
 
+    float horizontalInput;
+    float verticalInput;
 
-    public float speed = 5;
+    float speed = 10;
     //Vector3 StartScale = Vector3.one;
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         //StartScale = transform.localScale;
     }
-    void Update()
+    void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
-        Vector2 Movement = new Vector2(horizontalInput, 0f);
 
-        transform.Translate(Movement * speed * Time.deltaTime);
+        if (horizontalInput != 0)
+        {
+            rb.AddForce (new Vector2(horizontalInput * speed, 0f));
+        }
+        if (horizontalInput > 0)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (horizontalInput < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
 
