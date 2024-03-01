29949955dc4fce2e5091033ce7e809ce230d6234
Leo Zöger <leo.zoger@learnet.se>
2024-03-01 10:01:34 +0100

    Changes

--- Assets/DialogueSystem/DialogueActivator.cs
+++ Assets/DialogueSystem/DialogueActivator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class DialogueActivator : MonoBehaviour, IInteractable
 {
     [SerializeField] private DialogueObject dialogueObject;
-   public void Interact(Playercontroller player)
+   public void Interact(PlayerController player)
     {
       player.DiglogueUI.showDialogue(dialogueObject);
     }
