cee4b8db0f36925914beaf3e151e2aeb45e24ad6
Abas Yosef <abas.yosef@learnet.se>
2024-04-09 10:53:07 +0200

    spawner

--- Assets/enemySpawner.cs
+++ Assets/enemySpawner.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class enemySpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] enemyPrefabs; // Array av fiendens prefabs som ska spawnas
+    public float spawnInterval = 5.0f; // Intervall mellan varje spawn
+    public float spawnRadius = 5.0f; // Maximalt avstånd från spawner där fienden kan spawnas
 
-    // Update is called once per frame
+
+
+    private float timeSinceLastSpawn = 0.0f;
     void Update()
     {
-        
+        timeSinceLastSpawn += Time.deltaTime;
+        // Kontrollera om det är dags att spawn en ny fiende
+        if (timeSinceLastSpawn > spawnInterval)
+        {
+            SpawnEnemy();
+            timeSinceLastSpawn = 0.0f;
+        }
+
+
+    }
+
+    void SpawnEnemy()
+    {
+        // Välj en slumpmässig fiende från arrayen
+        GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
+        // Beräkna en slumpmässig position inom spawnradie runt spawnerns position
+        Vector2 spawnPosition = (Vector2)transform.position + Random.insideUnitCircle.normalized * spawnRadius;
+
+        // Skapa en fiende vid spawnpositionen
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+    void OnDrawGizmosSelected()
+    {
+        // Rita en gizmo för att visa spawnradie i scenen
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 }
