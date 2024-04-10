9e9fc2a0096860f2fbbf7e56fe6fd7925a483284
Almir Bajraktarevic <almir.bajraktarevic@learnet.se>
2024-04-10 12:27:56 +0200

    You can close game ig

--- Assets/Script/QuitGame.cs
+++ Assets/Script/QuitGame.cs
@@ -3,19 +3,10 @@ using UnityEngine.UI;
 
 public class QuitGame : MonoBehaviour
 {
-    public Button quitButton;
-
-    // Start is called before the first frame update
-    void Start()
+    
+    public void CloseGame()
     {
-        // Add a listener to the button click event
-        quitButton.onClick.AddListener(QuitGameFunction);
-    }
-
-    // Function to quit the game
-    void QuitGameFunction()
-    {
-        // Quit the application
+        Debug.Log("Quitting the game...");
         Application.Quit();
     }
 }
