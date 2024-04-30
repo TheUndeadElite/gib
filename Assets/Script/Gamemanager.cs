99a302c895b8cd007380eaa67c0a0c681214329f
Almir Bajraktarevic <almir.bajraktarevic@learnet.se>
2024-04-30 11:04:49 +0200

    volume slider (main menu)(incomplete)

--- Assets/Script/Gamemanager.cs
+++ Assets/Script/Gamemanager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Gamemanager : MonoBehaviour
 {
@@ -14,9 +17,15 @@ public class Gamemanager : MonoBehaviour
     
 
     [SerializeField] GameObject settingsBox;
+    [SerializeField] private AudioMixer Mixer;
+    [SerializeField] private AudioSource AudioSource;
+    [SerializeField] private TextMeshProUGUI ValueText;
+    [SerializeField] private AudioMixMode MixMode;
+
+    [SerializeField] private Slider volumeSlider;
     void Start()
     {
-        
+        volumeSlider.value = 50;
     }
 
     // Update is called once per frame
@@ -72,4 +81,44 @@ public class Gamemanager : MonoBehaviour
         LinearMixerVolume,
         LogrithmicMixerVolume
     }
+    public void OnChangeSlider(float Value)
+    {
+        ValueText.SetText($"{Value.ToString("N4")}");
+
+        switch (MixMode)
+        {
+            case AudioMixMode.LinearAudioSourceVolume:
+                // Convert the slider value from 0-100 range to 0-1 range
+                float linearVolume = Value / 100f;
+                AudioSource.volume = linearVolume;
+                break;
+            case AudioMixMode.LinearMixerVolume:
+                // Convert the slider value from 0-100 range to -80-20 range
+                float mixerVolumeLinear = -80 + Value;
+                Mixer.SetFloat("Volume", mixerVolumeLinear);
+                break;
+            case AudioMixMode.LogrithmicMixerVolume:
+                // Convert the slider value from 0-100 range to -80-20 range
+                float mixerVolumeLog = Mathf.Log10(Value / 100f) * 20;
+                Mixer.SetFloat("Volume", mixerVolumeLog);
+                break;
+        }
+
+        // Find the audio source component in the canvas
+        AudioSource canvasAudioSource = FindObjectOfType<AudioSource>();
+
+        // Mute the audio source if the slider value is close to 0
+        if (Mathf.Approximately(Value, 0f) && MixMode == AudioMixMode.LinearAudioSourceVolume && canvasAudioSource != null)
+        {
+            canvasAudioSource.mute = true;
+        }
+        else
+        {
+            // Unmute the audio source
+            canvasAudioSource.mute = false;
+        }
+    }
+
+
+
 }
