3b7f1d2a79a62e83ca0eb630468f1bee1dcae557
Abas Yosef <abas.yosef@learnet.se>
2024-05-21 11:03:53 +0200

    enemy spawer

--- Assets/Script/enemySpawner.cs
+++ Assets/Script/enemySpawner.cs
@@ -4,46 +4,66 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
     public float spawnRadius = 5.0f;
-    public int numberOfEnemiesToSpawn = 5;
 
-    bool canSpawn = false;
+    [HideInInspector]
+    public bool canSpawn = false;
+
+    [SerializeField] private int numberOfType1ToSpawn = 5;
+    [SerializeField] private int numberOfType2ToSpawn = 5;
 
     void Update()
     {
-        // Kolla om vi kan spawn och sedan spawnar fiender
         if (canSpawn)
         {
-            SpawnEnemy();
-            canSpawn = false; // Återställ flaggan efter spawn
+            SpawnEnemies();
+            canSpawn = false;
         }
     }
 
-    // När en kolliderare träffar den här spawneren
-    void OnTriggerEnter2D(Collider2D other)
+    // Uppdatera metoden för att ta emot två argument för antalet fiender av varje typ som ska spawna
+    public void ActivateSpawner(int type1Count, int type2Count)
     {
-        // Kolla om det är spelaren som träffar
-        if (other.CompareTag("Player"))
-        {
-            // Aktivera spawneren för att spawn fiender
-            canSpawn = true;
-        }
+        numberOfType1ToSpawn = type1Count;
+        numberOfType2ToSpawn = type2Count;
+        canSpawn = true;
     }
 
-    // Funktion för att spawn fiender
-    public void SpawnEnemy()
+    public void SpawnEnemies()
     {
-        for (int i = 0; i < numberOfEnemiesToSpawn; i++)
+        SpawnEnemyType(0, numberOfType1ToSpawn); // Spawn enemies of type 1
+        SpawnEnemyType(1, numberOfType2ToSpawn); // Spawn enemies of type 2
+    }
+
+    void SpawnEnemyType(int enemyTypeIndex, int count)
+    {
+        int prefabIndex = Mathf.Clamp(enemyTypeIndex, 0, enemyPrefabs.Length - 1);
+        GameObject enemyPrefab = enemyPrefabs[prefabIndex];
+
+        for (int i = 0; i < count; i++)
         {
-            GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
-            Vector2 spawnPosition = (Vector2)transform.position + Random.insideUnitCircle.normalized * spawnRadius;
+            Vector2 spawnPosition = (Vector2)transform.position + Random.insideUnitCircle * spawnRadius;
             Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
         }
     }
 
-    // Visar en visuell indikation av spawnradie i Unity-editorn
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, spawnRadius);
+    }
+
+    void Reset()
+    {
+        BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+        if (collider == null)
+        {
+            collider = gameObject.AddComponent<BoxCollider2D>();
+            collider.isTrigger = true;
+        }
+
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
     }
 }
